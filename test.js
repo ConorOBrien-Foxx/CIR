f0a046d5c5f605e2204001c90cff810763fed849
ConorOBrien-Foxx <conorobrien4god@gmail.com>
2023-05-08 01:02:33 -0400

    added organization

--- test.js
+++ test.js
@@ -1,5 +1,6 @@
 import fs from "fs";
-import { tokenize, makeTree } from "./tokenize.js";
+import { tokenize } from "./src/tokenize.js";
+import { makeTree } from "./src/tree-parse.js";
 
 // const text = fs.readFileSync("evaluate.cir");
 const text = fs.readFileSync("test.cir").toString();
